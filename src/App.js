5de876f0dbfa37a5cb9c3f67ef5c8b1fa541f120
nico <nicovg_95@hotmail.com>
2024-04-29 23:09:53 -0300

    dwada

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React, { useState } from 'react'
-import { stateContext } from './components/context/context'
-import Navbar from './components/Navbar';
-import Home from './components/Home/Home';
-import FirstDivision from './components/FirstDivision';
-import HowItWorks from './components/HowItWorks/HowItWorks';
-import SecondDivision from './components/SecondDivision'
-import WhySave from './components/WhySave/WhySave';
-import ThirstDivision from './components/ThirstDivision'
-import DownloadApp from './components/DownloadApp';
-import Footer from './components/Footer';
-import ContactPopUp from './components/ContactPopUp';
-import VideoDemo from './components/VideoDemo';
-import FFDivision from './components/FFDivision';
+  import React, { useState } from 'react'
+  import { stateContext } from './components/context/context'
+  import Navbar from './components/Navbar';
+  import Home from './components/Home/Home';
+  import FirstDivision from './components/FirstDivision';
+  import HowItWorks from './components/HowItWorks/HowItWorks';
+  import SecondDivision from './components/SecondDivision'
+  import WhySave from './components/WhySave/WhySave';
+  import ThirstDivision from './components/ThirstDivision'
+  import DownloadApp from './components/DownloadApp';
+  import Footer from './components/Footer';
+  import ContactPopUp from './components/ContactPopUp';
+  import VideoDemo from './components/VideoDemo';
+  import FFDivision from './components/FFDivision';
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";
 
-function App() {
-  const [contactPopUp, setContactPopUp] = useState(false)
+  function App() {
+    const [contactPopUp, setContactPopUp] = useState(false)
 
-  return (
-    <div>
-      <stateContext.Provider value={{contactPopUp, setContactPopUp}}>
-        <Navbar />
-        <Home />
-        <FFDivision />
-        <VideoDemo />
-        <FirstDivision />
-        <HowItWorks />
-        <SecondDivision />
-        <WhySave />
-        <ThirstDivision />
-        <DownloadApp />
-        {contactPopUp ? <ContactPopUp /> : null}
-        <Footer />
-      </stateContext.Provider>
-    </div>
-  );
-}
+    return (
+      <div>
+        <stateContext.Provider value={{contactPopUp, setContactPopUp}}>
+          <Navbar />
+          <Home />
+          <FFDivision />
+          <VideoDemo />
+          <FirstDivision />
+          <HowItWorks />
+          <SecondDivision />
+          <WhySave />
+          <ThirstDivision />
+          <DownloadApp />
+          {contactPopUp ? <ContactPopUp /> : null}
+          <Footer />
+        </stateContext.Provider>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
